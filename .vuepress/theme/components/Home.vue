0b2151246f4f1d44cb7ec32faf0dd445cf9a874f
Mike Pirog <mike@lando.dev>
2022-03-21 15:49:07 -0400

    Fix footer and improve homepage a bit

--- .vuepress/theme/components/Home.vue
+++ .vuepress/theme/components/Home.vue
@@ -7,23 +7,26 @@
       </div>
       <div class="home-content">
         <TagPage :pages="filteredContent" />
-        <div class="rightbar">
-          <h3>Topics</h3>
-          <div
-            v-if="tags.length > 0"
-            class="topic-list"
-          >
-            <a
-              v-for="tag in tags"
-              :key="tag.key"
-              class="topic-link"
-              @click="filterContent(tag.name)"
+        <div class="home-rightbar-wrapper">
+          <div class="home-rightbar">
+            <h3>Topics</h3>
+            <div
+              v-if="tags.length > 0"
+              class="topic-list"
             >
-              {{ tag.tag }}
-            </a>
+              <a
+                v-for="tag in tags"
+                :key="tag.key"
+                class="topic-link"
+                :class="{active: tag.name === filter}"
+                @click="filterContent(tag.name)"
+              >
+                {{ tag.tag }}
+              </a>
+            </div>
+            <h3>Search</h3>
+            <NavbarSearch />
           </div>
-          <h3>Search</h3>
-          <NavbarSearch />
         </div>
       </div>
     </main>
@@ -50,57 +53,103 @@ const filteredContent = computed(() => {
 });
 
 const filterContent = tag => {
-  filter.value = tag;
+  if (filter.value === tag) filter.value = null;
+  else filter.value = tag;
 };
 </script>
 
 <style lang="scss">
 @import './node_modules/@lando/vuepress-theme-default-plus/styles/main.scss';
-.home {
-  width: 900px;
-  #main-title {
-    display: none;
-  }
-}
-.tagline {
-  padding-top: 2em;
-  padding-bottom: 2em;
-  font-weight: 700;
-  font-size: 1.5em;
-}
-.home-content {
-  display: flex;
-  .tag-page {
-    width: var(--content-width);
-  }
-  .rightbar {
-    width: var(--rightbar-width);
-    padding-top: 0;
-    h3 {
-      padding-top: 0;
-      margin-top: 0;
+  .blog-home {
+    .home {
+    width: var(--homepage-width);
+    #main-title {
+      display: none;
     }
-    .search-box {
-      margin-left: 0;
-      width: 100%;
+    .tagline {
+      padding-top: 2em;
+      padding-bottom: 2em;
+      font-weight: 700;
+      font-size: 1.5em;
     }
-    .topic-link {
-      cursor: pointer;
-      color: var(--c-brand-light);
-      display: block;
+    .home-content {
+      display: flex;
+      .tag-page {
+        width: var(--content-width);
+      }
+      .home-rightbar {
+        width: var(--rightbar-width);
+        padding-top: 0;
+        transition: transform var(--t-transform), background-color var(--t-color), border-color var(--t-color);
+        width: var(--rightbar-width);
+        margin-left: var(--rightbar-margin);
+        padding-left: 0;
+        position: sticky;
+        top: var(--navbar-height);
+        > * {
+          margin-bottom: 1em;
+        }
+        h3 {
+          padding-top: 0;
+          margin-top: 0;
+        }
+        .search-box {
+          margin-left: 0;
+          width: 100%;
+        }
+        .topic-link {
+          cursor: pointer;
+          color: var(--c-text-lightest);
+          display: block;
+          &.active {
+            text-decoration: underline;
+            color: var(--c-brand-light);
+          }
+        }
+      }
     }
   }
 }
 
-@media (max-width: 1500px) {
-.home-content
-  .rightbar {
-    display: none;
+@media (max-width: 1000px) {
+  .blog-home {
+    .home-content {
+      .home-rightbar {
+        display: none;
+      }
+    }
+    .read-mode {
+      .page-wrapper-outer {
+        padding-top: 0;
+        padding-left: 0;
+      }
+    }
   }
-  .read-mode {
-    .page-wrapper-outer {
-      padding-top: 0;
-      padding-left: 0;
+}
+
+@media (max-width: $MQNarrow) {
+  .no-sidebar {
+    &.blog-home {
+      .page-wrapper-outer {
+        display: block;
+        padding: 2em;
+      }
+    }
+  }
+  .blog-home {
+    .home {
+      width: 100%;
+      padding: 0;
+    }
+    .page-wrapper-inner {
+      width: 100%;
+      display: block;
+    }
+    .page {
+      width: 100%;
+    }
+    .tag-page {
+      width: 100%;
     }
   }
 }
--- index.md
+++ index.md
@@ -2,6 +2,5 @@
 metaTitle: Home | Lando blog, the blog for professional developers
 home: true
 rightbar: true
+pageClass: blog-home
 ---
-
-erherh
